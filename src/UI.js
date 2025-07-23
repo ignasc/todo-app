9624eece6477684497388b313ed36673327503d3
Ignas <15948796+ignasc@users.noreply.github.com>
2025-07-23 15:13:34 +0100

    Add buttons to side bar that toggle project details.

--- src/UI.js
+++ src/UI.js
@@ -1,4 +1,4 @@
-import { projectGetAll } from "./dbActions";
+import { projectGetAll, projectRetrieve } from "./dbActions";
 
 const divSidebar = document.querySelector("#content-sidebar");
 const divMain = document.querySelector("#content-main");
@@ -39,14 +39,22 @@ function showProjectsSideBar(){
 
     const projectList = document.createElement("ul");
     const projectItem = document.createElement("li")
+    const btnProject = document.createElement("button");
     const projects = projectGetAll();
 
     for (let i = 0; i < projects.length; i++) {
         const element = projects[i];
         const newItem = projectItem.cloneNode();
-        newItem.textContent = element.title;
-        newItem.setAttribute("id", element.id)
+        const newButton = btnProject.cloneNode();
+        newButton.addEventListener("click", (e)=>{
+            projectRetrieve(e.target.id).toggleDetails();
+            updateUI();
+        });
+        newButton.textContent = element.title;
+        newButton.setAttribute("id", element.id)
+        newItem.setAttribute("data-id", element.id)
 
+        newItem.appendChild(newButton);
         projectList.appendChild(newItem);
     };
 
